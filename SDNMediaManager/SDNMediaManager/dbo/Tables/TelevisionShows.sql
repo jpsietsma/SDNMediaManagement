507820b1eb850961feb7583ea51afb50c34b817f
Jimmy S <jpsietsma@gmail.com>
2018-11-08 02:30:03 -0500

    Updated SQL project

--- SDNMediaManager/SDNMediaManager/dbo/Tables/TelevisionShows.sql
+++ SDNMediaManager/SDNMediaManager/dbo/Tables/TelevisionShows.sql
@@ -3,13 +3,15 @@
     [ShowName]         NVARCHAR (150) NOT NULL,
     [ShowDriveLetter]  NVARCHAR (1)   NOT NULL,
     [ShowHomePath]     NVARCHAR (200) NOT NULL,
-    [ShowNumSeasons]   INT            CONSTRAINT [DF_sdnTelevisionShows_ShowNumSeasons] DEFAULT ((0)) NOT NULL,
-    [ShowNumEpisodes]  INT            CONSTRAINT [DF_sdnTelevisionShows_ShowNumEpisodes] DEFAULT ((0)) NOT NULL,
+    [ShowNumSeasons]   INT            CONSTRAINT [DF_sdnTelevisionShows_ShowNumSeasons] DEFAULT ((0)) NULL,
+    [ShowNumEpisodes]  INT            CONSTRAINT [DF_sdnTelevisionShows_ShowNumEpisodes] DEFAULT ((0)) NULL,
     [ShowAlbumArtPath] NVARCHAR (200) CONSTRAINT [DF_sdnTelevisionShows_ShowAlbumArtPath] DEFAULT (N'~/tmp/img/0.jpg') NOT NULL,
-    [IsEnabled]        INT            CONSTRAINT [DF_sdnTelevisionShows_IsEnabled] DEFAULT ((1)) NOT NULL,
+    [IsEnabled]        NVARCHAR (5)   CONSTRAINT [DF_sdnTelevisionShows_IsEnabled] DEFAULT (N'true') NOT NULL,
     [TvdbID]           NVARCHAR (50)  NULL,
     [ImdbID]           NVARCHAR (50)  NULL,
     [fk_MediaType]     INT            NULL,
     CONSTRAINT [PK_sdnTelevisionShows] PRIMARY KEY CLUSTERED ([pk_ShowID] ASC)
 );
+
+
 
---
+++ SDNMediaManager/SDNMediaManager/dbo/Tables/list_MediaDrives.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[list_MediaDrives] (
+    [pk_MediaDriveID] INT           IDENTITY (1, 1) NOT NULL,
+    [DriveLetter]     CHAR (1)      NOT NULL,
+    [fk_MediaTypeID]  INT           NOT NULL,
+    [DriveSpace]      NVARCHAR (50) CONSTRAINT [DF_list_MediaDrives_DriveSpace] DEFAULT (N'unscanned') NOT NULL,
+    CONSTRAINT [PK_list_MediaDrives] PRIMARY KEY CLUSTERED ([pk_MediaDriveID] ASC),
+    CONSTRAINT [FK_list_MediaDrives_list_MediaTypes] FOREIGN KEY ([pk_MediaDriveID]) REFERENCES [dbo].[list_MediaTypes] ([pk_MediaTypeID])
+);
+
