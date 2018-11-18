172d2a2786284d6fd445b9a122b32750d6c0e5fb
Jimmy S <jpsietsma@gmail.com>
2018-11-18 15:34:19 -0500

    Fixed Autocomplete season and episode totals.

---
+++ SDNMediaModels/UI/AutocompleteResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDNMediaModels.UI
+{
+    public class AutocompleteResult
+    {
+        public string ShowName { get; set; }
+        public string ShowNumSeasons { get; set; }
+        public string ShowNumEpisodes { get; set; }
+
+        public AutocompleteResult(string showName, string numSeasons, string numEpisodes)
+        {
+            ShowName = showName;
+            ShowNumSeasons = numSeasons;
+            ShowNumEpisodes = numEpisodes;
+        }
+    }
+}
