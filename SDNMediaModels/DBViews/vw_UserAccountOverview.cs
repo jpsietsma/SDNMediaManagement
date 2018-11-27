b9e3293a9e8a1280390b4bc5877e934daf6f9b00
Jimmy S <jpsietsma@gmail.com>
2018-11-27 02:08:09 -0500

    Completed XML comments for MediaManagerDB.Context.cs, SDNMediaModels.DBViews

--- SDNMediaModels/DBViews/vw_UserAccountOverview.cs
+++ SDNMediaModels/DBViews/vw_UserAccountOverview.cs
@@ -4,10 +4,25 @@ using System.Collections.Generic;
 namespace SDNMediaModels.DBViews
 {    
 
+    /// <summary>
+    /// View giving permissions overview of all system accounts
+    /// </summary>
     public partial class vw_UserAccountOverview
     {
+
+        /// <summary>
+        /// Login user name associated with user account
+        /// </summary>
         public string UserName { get; set; }
+
+        /// <summary>
+        /// Email addres associated with user account
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Name of Role granted to user account
+        /// </summary>
         public string Name { get; set; }
     }
 }
